7babc01967251d176b8c4258fdced5748f140617
sathish1980 <kumar.sathish189@gmail.com>
2022-06-26 11:16:48 +0530

    newchanges

--- Sampletest/sample - Copy.py
+++ Sampletest/sample - Copy.py
@@ -2,6 +2,9 @@ class sample():
 
     def test1(self):
         print("this is test")
+        
+    def test2(self):
+        print("this is done by student")
 
     def test2(self):
         print("this is test")
